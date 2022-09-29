bb9d2e52694f3bae08edbb068fdc7451bf2ea1d8
Jelani Cannon <jelani.cannon@gmail.com>
2022-09-28 17:13:22 -0700

    commit tree 6be5ca7feb29809ece87b0b29a767f0c3bd9b3bc

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching (drivers, name ){
+    let found = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase() )
+    return found
+}
+
+function fuzzyMatch(drivers, init){
+    let match = drivers.filter(driver => driver[0] === init[0])
+    return match
+}
+
+function matchName (drivers, name){
+    let foundDriver = drivers.filter(function (driver){
+        let found = driver.name === name
+        return found
+    })
+    return foundDriver
+}
